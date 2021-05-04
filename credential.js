9829f1ec9719c0f53cc3c4ddccb253250379e29d
EnzoReverdi <enzo.reverdi@epitech.eu>
2021-05-04 18:07:40 +0200

    Init

---
+++ credential.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ["--user-data-dir=./Google/Chrome/User Data/"],
+    headless: false,
+    executablePath: '/bin/google-chrome'
+  });
+  const page = await browser.newPage();
+  await page.goto('https://my.epitech.eu/index.html#2021/all/all');
+  const elements = await page.$x('/html/body/div/div/a/span');
+  if (elements[0]) {
+    await elements[0].click();
+  }
+})();
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+npm i
+echo "#!/bin/bash
+INSTALL_DIR=\"$(pwd)\"
+cd \$INSTALL_DIR
+TOKEN=\$(node token_fetcher.js)
+PRETOK=\"Authorization: Bearer \"
+curl -sH 'Accept: application/json' -H \"\${PRETOK}\${TOKEN}\" https://api.epitest.eu/me/2020 > buffer
+PROJECT=\$(cat buffer | jq '.[-1][\"project\"][\"slug\"]')
+NBTEST=\$(cat buffer | jq '.[-1]' | sed -n 's/.*\"count\": \([0-9]*\).*/\1/p' | paste -sd+ | bc)
+TESTPASSED=\$(cat buffer | jq '.[-1]' | sed -n 's/.*\"passed\": \([0-9]*\).*/\1/p' | paste -sd+ | bc)
+printf \"  \$(python -c \"print(\${TESTPASSED} / \${NBTEST} * 100)\")%% \${PROJECT}  \n\"
+rm -rf buffer" > NLKELS.sh
+chmod 755 NLKELS.sh
+node credentials.js
